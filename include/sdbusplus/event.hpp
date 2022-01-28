a35305104b4b84d2e3cdecf0302673844f2bddd0
George Liu <liuxiwei@inspur.com>
2022-01-28 09:34:48 +0800

    13
    Signed-off-by: George Liu <liuxiwei@inspur.com>

--- include/sdbusplus/event.hpp
+++ include/sdbusplus/event.hpp
@@ -18,6 +18,13 @@ struct events
 {
     virtual const char* name() const noexcept = 0;
     virtual const char* description() const noexcept = 0;
+
+  private:
+    // This unused function is to ensure that the vtable for this class is
+    // properly emitted when `-flto=auto` is used, which is the default in
+    // Yocto builds.  Without this, the vtable is a hidden symbol and no
+    // users can inherit from our exception type directly.
+    virtual void unused() const noexcept;
 };
 
 } // namespace events
